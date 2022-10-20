e828fc01dd1d6d42fa2fa588eec9df024cea5130
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2022-10-21 04:51:34 +0700

    Add files via upload

--- Praktikum 6/No 1/No 1.php
+++ Praktikum 6/No 1/No 1.php
@@ -1,1 +1,37 @@
+<?php 
 
+require_once 'No 1 Abstract.php';
+
+class Truck extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+    
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+        
+        return ceil($fuel /= $trip);
+    }
+}
+
+class RiverBarge extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+
+        return ceil($fuel /= $trip);
+    }
+}
+$truck = new Truck(16000, 'Truk');
+$riverBarge = new RiverBarge(15000, 'Perahu');
